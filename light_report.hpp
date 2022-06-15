05ef233c33b51d90ef14dfd717a114028ce0ff19
2Nagyeom <70017526+2Nagyeom@users.noreply.github.com>
2022-06-15 14:27:52 +0900

    조명, 음영 레포트, 시간에 따라 조명효과 다르게 보이기

---
+++ light_report.hpp
@@ -0,0 +1,13 @@
+//
+//  light_report.hpp
+//  openGL
+//
+//  Created by 이나겸 on 2022/06/15.
+//
+
+#ifndef light_report_hpp
+#define light_report_hpp
+
+#include <stdio.h>
+
+#endif /* light_report_hpp */
